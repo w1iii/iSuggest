f77e36c2ebea7221e988ee5172c08776fa12a7be
Daniel Ramos <ramosdaniel1186@gmail.com>
2026-06-09 12:48:32 +0800

    Fixed the image not updating

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -7,10 +7,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    protected $appends = ['profile_image_url'];
 
     protected $fillable = [
         'name',
